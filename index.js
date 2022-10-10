5ae1a30ef2c26e6a6263491c13bc8f5eaa1aaa2e
Ania pq <melanpq@gmail.com>
2022-10-10 10:29:15 -0500

    Updates

--- index.js
+++ index.js
@@ -1,61 +1,216 @@
 import validator from "./validator.js";
 
-const $button = document.getElementById("validar");
-$button.addEventListener("click", inputvalue);
+//guardo mis input del DOM
+const $numeroTarjeta = document.getElementById("card_number");
+const $nombre = document.getElementById("card_nombre");
+const $fechaVencimiento = document.getElementById("card_fecha");
+const $ccv = document.getElementById("card_ccv");
+const $boton = document.getElementById("validar");
 
-function inputvalue() {
-  let creditCardNumber = document.getElementById("card_number").value;
+// mis eventos:n
+window.addEventListener("load", function () {
+  document.getElementById("mensaje").style.visibility = "hidden";
+  document.getElementById("mensaje1").style.visibility = "hidden";
+  document.getElementById("mensaje2").style.visibility = "hidden";
+  document.getElementById("mensaje3").style.visibility = "hidden";
+});
+
+$numeroTarjeta.addEventListener("blur", validaNumero);
+$fechaVencimiento.addEventListener("blur", validaFecha);
+$ccv.addEventListener("blur", validaCcv);
+$nombre.addEventListener("blur", validaNombre);
+$boton.addEventListener("click", resultado);
+
+//funcion para limpiar mis valores de los input
+/*
+function limpiarInputs() {
+  $numeroTarjeta.value == "";
+  $nombre.value == "";
+  $fechaVencimiento.value == "";
+  $ccv.value == "";
+}*/
+
+//funcion para validar entradas en los input
+//1.VALIDA NUMEROS
+function validaNumero() {
+  let creditCardNumber = $numeroTarjeta.value;
   const $remplazar = document.getElementById("resultado");
-  const $texto = document.getElementById("card_number");
 
-  if (validator.isValid(creditCardNumber) == true) {
-    $texto.value = validator.maskify(creditCardNumber);
-    $remplazar.outerHTML =
-      '<p id="resultado">CORRECTO,la tarjeta es valida</p>';
+  let valoresAceptados = /^[0-9]+$/;
+  if (creditCardNumber.match(valoresAceptados)) {
+    document.getElementById("mensaje").style.visibility = "hidden";
+
+    //alert("Es numérico");
+    // busca,mos el resultado con validator--------------------------------------------------------
+    if (validator.isValid(creditCardNumber)) {
+      document.getElementById("numero_tarjeta").innerHTML = creditCardNumber;
+      $numeroTarjeta.value = validator.maskify(creditCardNumber);
+      $remplazar.outerHTML =
+        '<p id="resultado">✔️CORRECTO,la tarjeta es valida</p>';
+      document.getElementById("resultado").style.backgroundColor = "#198754";
+
+      // lo esscribo en la tarjeta
+    } else {
+      document.getElementById("numero_tarjeta").innerHTML = creditCardNumber;
+      $numeroTarjeta.value = validator.maskify(creditCardNumber);
+      $remplazar.outerHTML =
+        '<p id="resultados">❌INCORRECTO,la tarjeta NO es valida</p>';
+      document.getElementById("resultados").style.backgroundColor = "#dc3545";
+      document.getElementById("mensaje").style.visibility = "visible";
+    }
+
+    //------------------------------------------------------------
   } else {
-    $texto.value = validator.maskify(creditCardNumber);
-    $remplazar.outerHTML =
-      '<p id="resultado">INCORRECTO,la tarjeta NO es valida</p>';
+    document.getElementById("card_number").value = "";
+
+    document.getElementById("mensaje").innerHTML =
+      " ⚠️Valor no permitido,acepta solo números";
+    document.getElementById("mensaje").style.visibility = "visible";
+    //alert("No es numérico");
   }
 }
 
-/* para la ventana pequeña*/
-const $cerrar = document.querySelector(".cerrar");
-const $abrir = document.getElementById("validar");
-const $ventana = document.querySelector(".ventana");
-const $ventanaP = document.querySelector(".ventana-contenedor");
+//2.VALIDA FECHAS
 
-$abrir.addEventListener("click", function () {
-  $ventanaP.style.opacity = "1";
-  $ventanaP.style.visibility = "visible";
-  $ventana.classList.toggle("ventana-cerrar");
-});
+function validaFecha() {
+  let creditCardDate = $fechaVencimiento.value;
+  let reg = new RegExp(
+    "(((0[123456789]|10|11|12)/(([1][9][0-9][0-9])|([2][0-9][0-9][0-9]))))"
+  );
 
-$cerrar.addEventListener("click", function () {
-  $ventana.classList.toggle("ventana-cerrar");
+  if (reg.test(creditCardDate) == true) {
+    document.getElementById("mensaje2").style.visibility = "hidden";
+    // alert("soy fecha");
+    document.getElementById("fecha").innerHTML = creditCardDate;
+  } else {
+    document.getElementById("card_fecha").value = "";
 
-  setTimeout(function () {
-    $ventanaP.style.opacity = "0";
-    $ventanaP.style.visibility = "hidden";
-  }, 500);
-});
-/*el siguiente es para que cuando haga click fuera de la ventana tambien se cerrara*/
+    document.getElementById("mensaje2").innerHTML =
+      " ⚠️Valor No permitido. Ejem: MM/YYYY";
 
-window.addEventListener("click", function (e) {
-  if (e.target == $ventanaP) {
+    document.getElementById("mensaje2").style.visibility = "visible";
+    //alert("no soy fecha");
+  }
+}
+
+//NOMBRE
+
+//3.VALIDAR CVV
+function validaCcv() {
+  let creditCardCcv = $ccv.value;
+  let valoresAceptados = /^[1-9]+$/;
+  if (creditCardCcv.match(valoresAceptados)) {
+    document.getElementById("mensaje3").style.visibility = "hidden";
+    document.getElementById("ccv").innerHTML = creditCardCcv;
+
+    //alert("correcto");
+  } else {
+    document.getElementById("card_ccv").value = "";
+    document.getElementById("mensaje3").innerHTML =
+      " ⚠️Valor No permitido,acepta solo números";
+    document.getElementById("mensaje3").style.visibility = "visible";
+    //alert("incorrecto");
+  }
+}
+
+//4.VALIDAR NOMBRE CLIENTE
+function validaNombre() {
+  let creditCardName = $nombre.value;
+  let valoresAceptados = /[a-zA-ZÀ-ÖØ-öø-ÿ]+\.?(( |\-)[a-zA-ZÀ-ÖØ-öø-ÿ]+\.?)/;
+  if (creditCardName.match(valoresAceptados)) {
+    document.getElementById("mensaje1").style.visibility = "hidden";
+    document.getElementById("nombre_cliente").innerHTML = creditCardName;
+
+    //alert("correcto");
+  } else {
+    document.getElementById("nombre_cliente").value = "";
+    document.getElementById("mensaje1").innerHTML =
+      " ⚠️Valor invalido.Ejem: Melania Palomino";
+    document.getElementById("mensaje1").style.visibility = "visible";
+    //alert("incorrecto");
+  }
+}
+
+function resultado() {
+  // declaro variables para manejar otra ventana
+  const $cerrar = document.querySelector(".cerrar");
+  const $abrir = document.getElementById("validar");
+  const $ventana = document.querySelector(".ventana");
+  const $ventanaP = document.querySelector(".ventana-contenedor");
+
+  $abrir.addEventListener("click", function () {
+    $ventanaP.style.opacity = "1";
+    $ventanaP.style.visibility = "visible";
+    // para que lo carge de nuevo
+    $ventana.classList.toggle("ventana-cerrar");
+  });
+
+  $cerrar.addEventListener("click", function () {
+    // para que lo carge de nuevo
     $ventana.classList.toggle("ventana-cerrar");
 
     setTimeout(function () {
       $ventanaP.style.opacity = "0";
       $ventanaP.style.visibility = "hidden";
     }, 500);
-  }
-});
+    /*
+      location.reload();
+      $numeroTarjeta.value == "";
+      $nombre.value == "";
+      $fechaVencimiento.value == "";
+      $ccv.value == "";
+      */
+  });
+  //el siguiente es para que cuando haga click fuera de la ventana tambien se cerrara
 
-/*const input = document.querySelector("input");
-const log = document.getElementById("valores");
+  window.addEventListener("click", function (e) {
+    if (e.target == $ventanaP) {
+      $ventana.classList.toggle("ventana-cerrar");
 
-input.addEventListener("input", updateValue); 
+      setTimeout(function () {
+        $ventanaP.style.opacity = "0";
+        $ventanaP.style.visibility = "hidden";
+      }, 500);
+    }
+    //location.reload();
+  });
+}
+// ----------------llamo al objeto validator------------------
 
-function updateValue(e) {
-  log.textContent = e.srcElement.value;}*/
+//cod:ascci
+//a-z:97-122
+//A-Z:65-90
+//0-9:48-57
+//ñ:164 Ñ:165
+//espacio:32
+// balla inclinada(/):47
+
+//isNaN(valor)intenta convertir el valor pasado a un número. Si el  valor no se puede convertir, devuelve true; en caso contrario, devuelve false
+//isNaN(NaN); //devuelve true
+//isNaN("string"); //devuelve true
+//isNaN("12"); //devuelve false
+//isNaN(12); //devuelve false
+/*
+  console.log(typeof creditCardNumber);
+  console.log(parseInt(creditCardNumber));
+  console.log(creditCardNumber);
+
+  console.log(typeof creditCardNumber * 1);
+  console.log(parseInt(creditCardNumber * 1));
+  console.log(creditCardNumber * 1);
+}*/
+
+//onkeyup:ejecuta cuandoun usuario suelta la tecla
+//onkeydown:Ejecuta cuando un usuario presiona una tecla
+//onkeypress:El evento onkeypress ocurre cuando el usuario presiona una tecla (en el teclado)
+//onblur:Ejecute un JavaScript cuando un usuario abandone un campo de entrada
+//onfocus:Ejecute un JavaScript cuando un campo de entrada obtenga el foco
+//onfocusout:Ejecuta un JavaScript cuando un campo de entrada esté a punto de perder el foco
+//oninput:Ejecute un JavaScript cuando un usuario escriba algo en un campo <input>
+//oninvalid:Alerta algún texto si un campo de entrada no es válido
+//onload:Ejecute un JavaScript inmediatamente después de que se haya cargado una página
+//onselect:Ejecute un JavaScript cuando se haya seleccionado algún texto:
+//ontoggle:Ejecute un JavaScript cuando se abra o cierre un elemento <details>
+//onsubmit: Ejecute un JavaScript cuando se envíe un formulario
+//event.key: Obtenga el botón del teclado que se presionó cuando ocurrió un evento clave
+//event.target:Obtenga el elemento que desencadenó un evento específico
--- validator.js
+++ validator.js
@@ -5,32 +5,32 @@ const validator = {
     const arrayI = creditCardNumber.split("");
     const array_nuevo = [];
     //alert(arrayI);
-    console.log(`recibo del ${arrayI}`);
+    //console.log(`recibo del ${arrayI}`);
 
     for (let i = 0; i < arrayI.length; i++) {
       // confirm(arrayI[i]);
       if (i % 2 === 1) {
         // Ingresan los que son de posicion impar y el cero
-        let suma = arrayI[i] * 2;
+        let suma = parseInt(arrayI[i]) * 2;
         if (suma >= 10) {
           array_nuevo.push(suma - 9);
         } else {
           array_nuevo.push(suma);
         }
-        console.log(`digito=${suma}:suma=${suma - 9}`);
+        //console.log(`digito=${suma}:suma=${suma - 9}`);
       } else {
-        array_nuevo.push(arrayI[i]);
+        array_nuevo.push(parseInt(arrayI[i]));
       }
     }
-    console.log(`arreglo nuevo ${array_nuevo}`);
+    // console.log(`arreglo nuevo ${array_nuevo}`);
 
     let result = 0;
     let sale;
 
     for (let x = 0; x < array_nuevo.length; x++) {
-      result = result + parseInt(array_nuevo[x]);
+      result = result + array_nuevo[x];
     }
-    console.log(` es:${result}`);
+    //console.log(` es:${result}`);
     sale = result % 10 === 0 ? true : false;
 
     return sale;
@@ -46,9 +46,32 @@ const validator = {
         arr.push(cardValue[i]);
       }
     }
+
     //alert(cardValue);
     return arr.join("");
   },
+
+  franchise(creditCardNumber) {
+    let anyString = creditCardNumber;
+
+    const arra4 = ["1800", "2014", "2131", "2149", "6011"];
+    const arra3 = ["300", "301", "302", "303", "304", "305"];
+    const arra2 = ["34", "36", "37", "38", "51", "52", "53", "54", "55"];
+    const arra1 = ["3", "4"];
+
+    //para los que empiezan en 4 cifras 1800 2014 2131 2149
+    for (let index = 0; index < arra4.length; index++) {
+      if (anyString.substring(0, 4) === arra4[index]) {
+      }
+    }
+
+    //para los que empiezan en 3 cifras 300-305
+
+    //para los que  empiezan en dos cifras34 36 37 38 51 52 53 54 55
+    //para los que empiezan en una cifra 3 4
+  },
 };
 // al objeto  validator anteponemos export y default  para usarla como objeto por defecto cuando lo importemos.
+
+validator.isValid("4213550123670241");
 export default validator;
