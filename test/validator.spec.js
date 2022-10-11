bae9167dc678cc745e68d54700ea640aaabfe31d
Ania pq <melanpq@gmail.com>
2022-10-11 10:44:05 -0500

    implementado la funcion de la franquicia para el objeto validator

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -44,4 +44,58 @@ describe("validator", () => {
       expect(validator.maskify("helloworld")).toBe("######orld");
     });
   });
+
+  describe("validator.franchise", () => {
+    it("debería ser una función", () => {
+      expect(typeof validator.maskify).toBe("function");
+    });
+
+    it('Debería retornar "Visa" para "4556364607935616"', () => {
+      expect(validator.franchise("4556364607935616")).toBe("Visa");
+    });
+    it('Debería retornar "" para "7776364607"', () => {
+      expect(validator.franchise("7776364607")).toBe("");
+    });
+    it('Debería retornar "JCB" para "18003646072541"', () => {
+      expect(validator.franchise("18003646072541")).toBe("JCB");
+    });
+    it('Debería retornar "Diner\'s Club / Carte Blanche" para "3046072545121741"', () => {
+      expect(validator.franchise("3046072545121741")).toBe(
+        "Diner's Club / Carte Blanche"
+      );
+    });
+    it('Debería retornar "American Express" para "37460728765554"', () => {
+      expect(validator.franchise("37460728765554")).toBe("American Express");
+    });
+
+    it('Debería retornar "MasterCard" para "55854254857578"', () => {
+      expect(validator.franchise("55854254857578")).toBe("MasterCard");
+    });
+    it('Debería retornar "Discover" para "601152544887441"', () => {
+      expect(validator.franchise("601152544887441")).toBe("Discover");
+    });
+    it('Debería retornar "Diner\'s Club / enRoute" para "2014525484587445"', () => {
+      expect(validator.franchise("2014525484587445")).toBe(
+        "Diner's Club / enRoute"
+      );
+    });
+    it('Debería retornar "Diner\'s Club / International" para "3612598561225"', () => {
+      expect(validator.franchise("3612598561225")).toBe(
+        "Diner's Club / International"
+      );
+    });
+    it('Debería retornar "JCB" para "3378541254215444"', () => {
+      expect(validator.franchise("3378541254215444")).toBe("JCB");
+    });
+
+    it('Debería retornar "Diner\'s Club / Carte Blanche" para "38251254896356221"', () => {
+      expect(validator.franchise("38251254896356221")).toBe(
+        "Diner's Club / Carte Blanche"
+      );
+    });
+
+    it('Debería retornar "JCB" para "21318548754296"', () => {
+      expect(validator.franchise("21318548754296")).toBe("JCB");
+    });
+  });
 });
